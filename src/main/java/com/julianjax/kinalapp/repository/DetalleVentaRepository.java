f938472a5694980b36063f76234f6411370f0574
jjax-2025549 <jjax-2025549@kinal.edu.gt>
2026-03-27 00:56:43 -0600

    feat: crear DetalleVentaRepository

--- src/main/java/com/julianjax/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/julianjax/kinalapp/repository/DetalleVentaRepository.java
@@ -1,4 +1,10 @@
 package com.julianjax.kinalapp.repository;
 
-public interface DetalleVentaRepository {
+import com.julianjax.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+
 }
